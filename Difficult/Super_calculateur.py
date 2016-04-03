d7854b71e778103ca14a488cc80e436aff46389b
Yann HUET <Alumet@users.noreply.github.com>
2016-04-03 15:38:39 +0200

    Create Super_calculateur.py
    www.codingame.com

---
+++ Difficult/Super_calculateur.py
@@ -0,0 +1,19 @@
+
+n = int(input())
+liste=[]
+for i in range(n):
+    j, d = [int(j) for j in input().split()]
+    liste.append([j,j+d-1])
+    
+liste.sort(key=lambda x: x[0])
+liste.sort(key=lambda x: x[1])
+
+J_max=0
+count=0
+
+for el in liste:
+    if el[0]>J_max:
+        J_max=el[1]
+        count+=1
+      
+print(count)
