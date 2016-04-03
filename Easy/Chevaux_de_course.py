179e609506c8e6cb8a4eadb2c971445af5fa567b
Yann HUET <Alumet@users.noreply.github.com>
2016-04-03 14:05:27 +0200

    Create Chevaux_de_course.py
    www.codingame.com

---
+++ Easy/Chevaux_de_course.py
@@ -0,0 +1,15 @@
+n = int(input())
+power=[]
+
+for i in range(n):
+    pi = int(input())
+    power.append(pi)
+
+power.sort()
+
+best=abs(power[1]-power[0])
+
+for i in range(n-1):
+    best = min( best, abs(power[i]-power[i+1]))
+
+print (best)
