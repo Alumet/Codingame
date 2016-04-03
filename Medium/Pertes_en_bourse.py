eab5e0989c09b93aca5b9da1368ede8f99dff0fb
Yann HUET <Alumet@users.noreply.github.com>
2016-04-03 14:51:59 +0200

    Create Pertes_en_bourse.py
    www.codingame.com

---
+++ Medium/Pertes_en_bourse.py
@@ -0,0 +1,12 @@
+
+n = int(input())
+values = [int(x) for x in input().split(" ")]
+    
+loss,Max=0,0
+
+for price in values:
+
+    Max = max(Max, price)
+    loss = min(loss, price - Max)
+    
+print(loss)
